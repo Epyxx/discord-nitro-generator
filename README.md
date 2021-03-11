672db2e5c0fa6eeea5047331947aa32eaac5e9f2
Epyxx <60779416+Epyxx@users.noreply.github.com>
2021-03-11 16:22:23 +0100

    v1.0

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# discord-nitro-generator
-Epyxx' Discord Nitro Generator. Fragen bitte per Discord: Epyx#4545
+## Über das Projekt
+
+Ein einfacher Zufallsgenerator im Discord Nitro Code Format mit eingebauter Prüfung über Discords Verification API
+
+
+## Los geht's
+
+Um mit dem Tool zu starten, musst du nur die folgenden Schritte beachten
+
+### Voraussetzungen
+
+`node.js`
+
+### Installation unter Windows
+
+Schritt 1) Download und entpacke die .zip Datei
+
+Schritt 2) Gehe zu https://nodejs.org/de/download/ lade Node.js LTS für Windows herunter und installiere es
+
+Schritt 3) Führe die Datei "install_and_run.bat" aus und warte bis die nodejs Module installiert wurden
+
+Schritt 4) Führe die Datei "start.bat" aus
+
+Schritt 5) Füge deine HTTP Proxy Liste in die Datei proxies.txt ein. Falls keine Proxy Liste vorhanden ist, empfehle ich eine Liste von https://proxyscrape.com/free-proxy-list
+
+Schritt 6) Folge den Anweisungen und Abfragen in der CMD und starte den Generator!
+
+### Installation unter MacOS und Linux
+
+Schritt 1) Download und entpacke die .zip Datei
+
+Schritt 2) Gehe zu https://nodejs.org/de/download/ lade Node.js LTS für dein Betriebssystem herunter und installiere es
+
+Schritt 3) Öffne ein Terminal und wechsle zum entpackten Ordner. Gib "npm i" ein oder rechtsklicke auf den Ordner und wähle "New terminal at folder" und warte bis die nodejs Module installiert wurden
+
+Schritt 4) Öffne ein Terminal und wechsle zum entpackten Ordner. Gib "node app.js" ein oder rechtsklicke auf den Ordner und wähle "New terminal at folder"
+
+Schritt 5) Füge deine HTTP Proxy Liste in die Datei proxies.txt ein. Falls keine Proxy Liste vorhanden ist, empfehle ich eine Liste von https://proxyscrape.com/free-proxy-list
+
+Schritt 6) Folge den Anweisungen und Abfragen in der CMD und starte den Generator!
+
+
+## Lizenz
+
+Siehe `LICENSE` für weitere Informationen.
+
+## Kontakt
+Bei Problemen oder Fragen kontaktiere mich über Discord unter Epyx#4545
+
+Projekt Link: [https://github.com/epyxx/discord-nitro-generator/](https://github.com/epyxx/discord-nitro-generator/)
+
+# Disclaimer
+
+Dieses Tool wurde für Forschungs- und Lernzwecke entwickelt. Der Autor übernimmt KEINE Haftung dafür wie das Tool verwendet wird. Der Autor übernimmt keine Haftung für entstandene Schäden. Beim verwenden dieses Tools bestätigt der Benutzer dieses auf eigene Gefahr zu nutzen.
---
+++ app.js
@@ -0,0 +1,470 @@
+const request = require("request");
+const fs = require("fs");
+const figlet = require("figlet");
+const fetch = require("node-fetch");
+const lineReader = require('line-reader');
+const proxies = __dirname + "/proxies.txt";
+require("colors");
+var term = require("terminal-kit").terminal;
+var proxyLine = 0;
+var proxyUrl = "";
+var working = [];
+var version = "v1.0"
+var toMatch;
+// highest rate possible before the stress errors will start to occur
+const triesPerSecond = 1;
+
+console.clear();
+console.log(figlet.textSync("Nitro Generator").green);
+console.log(figlet.textSync("by Epyxx").red);
+
+
+generatecode = function() {
+    let code = "";
+    let dict = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    for (var i = 0; i < 24; i++) {
+        code = code + dict.charAt(Math.floor(Math.random() * dict.length));
+    }
+    return code;
+};
+// async for fetch
+async function updateLine() {
+    proxyLine++;
+    var readLine = 0;
+    lineReader.eachLine(proxies, function(line, last) {
+        readLine++;
+        if (readLine === proxyLine) {
+            proxyUrl = "http://" + line;
+        }
+        if (last) {
+            // scrape proxies if none are detected
+            readLine = 0;
+            if (proxyUrl === `http://${line}`) {
+
+                (async() => {
+                    term.cyan("Keine Proxy-Server gefunden. Hole neue...\n");
+                    await fetch("https://api.proxyscrape.com/v2/?request=displayproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=all").then(async res => {
+                        const body = (await res.text());
+                        fs.writeFileSync(__dirname + "/proxies.txt", body);
+                    });
+                })();
+                proxyLine = 0
+            }
+        }
+    });
+}
+
+updateLine();
+// requests api checks in order using proxies instead of all at the same time 
+// because it would be the exact same waiting time plus more stress with many requests at the same time possibly causing an error
+// also changed body requests to no longer parse body data and just use status codes instead
+checkCode = function(code) {
+    var proxiedRequest = request.defaults({
+        'proxy': proxyUrl
+    });
+    proxiedRequest.timeout = 1500;
+    proxiedRequest.get(`https://discordapp.com/api/v6/entitlements/gift-codes/${code}?with_application=false&with_subscription_plan=true`, (error, resp, body) => {
+        if (error) {
+            term.brightYellow("Ungültiger Proxy-Server. Wechsle...\n");
+            updateLine();
+            return;
+        }
+        try {
+            if (body.code == 200) {
+                term.brightGreen(`Dieser Code sollte funktionieren, außer du siehst eine Fehlermeldung! https://discord.gift/${code}\n`);
+                console.log(JSON.stringify(body, null, 4));
+                working.push(`https://discord.gift/${code}`);
+                fs.writeFileSync(__dirname + '/codes.json', JSON.stringify(working, null, 4));
+                if (toMatch === 0) {
+                    process.exit();
+                } else {
+                    //console.log("test")
+                }
+            } else if (body.code == 429) {
+                updateLine();
+                term.brightYellow("Proxy Limit erreicht! Wechsle...\n");
+
+            } else {
+                term.brightRed(`discord.com/gifts/${code} ist ein ungültiger Code!\n`);
+            }
+        } catch (error) {
+            term.gray("Ein Fehler ist aufgetreten:\n");
+            term.gray(error + "\n");
+            return;
+        }
+    });
+}
+checkCodeOffline = function(code) {
+    request(`https://discordapp.com/api/v6/entitlements/gift-codes/${code}?with_application=false&with_subscription_plan=true`, (error, res, body) => {
+        if (error) {
+            term.gray("Ein Fehler ist aufgetreten:\n");
+            term.gray(error + "\n");
+            return;
+        }
+        try {
+            if (body.code == 200) {
+                term.brightGreen(`Dieser Code sollte funktionieren, außer du siehst eine Fehlermeldung! https://discord.gift/${code}\n`);
+                console.log(JSON.stringify(body, null, 4));
+                working.push(`https://discord.gift/${code}`);
+                fs.writeFileSync(__dirname + '/codes.json', JSON.stringify(working, null, 4));
+                if (toMatch === 0) {
+                    process.exit();
+                } else {
+                    //console.log("test")
+                }
+            } else if (body.code == 429) {
+                term.brightYellow("Proxy Limit erreicht!");
+            } else {
+                term.brightRed(`discord.com/gifts/${code} ist ein ungültiger Code!\n`);
+
+            }
+        } catch (error) {
+            term.gray("Ein Fehler ist aufgetreten:\n");
+            term.gray(error + "\n");
+            return;
+        }
+    });
+}
+
+
+function main() {
+    term.brightYellow(
+        "Möchtest du den Nitro Generator starten? [Y|N]\n"
+    );
+
+    term.yesOrNo({
+        yes: ["y", "ENTER"],
+        no: ["n"]
+    }, function(error, result) {
+
+
+        if (result) {
+
+
+            term.brightYellow(
+                "Möchtest du Autostop aktivieren? [Y|N]\n"
+            );
+
+            term.yesOrNo({
+                yes: ["y", "ENTER"],
+                no: ["n"]
+            }, function(error, result) {
+                if (result) {
+                    toMatch = 0;
+
+                    term.brightYellow(
+                        "Möchtest du Proxy-Server benutzen? [Y|N]\n"
+                    );
+
+                    term.yesOrNo({
+                        yes: ["y", "ENTER"],
+                        no: ["n"]
+                    }, function(error, result) {
+                        if (result) {
+
+
+
+                            term.cyan("Benutze Proxies...\n");
+
+                            var progressBar, progress = 0;
+
+
+                            function doProgress() {
+
+                                // Add random progress
+                                progress += Math.random() / 10;
+                                progressBar.update(progress);
+
+                                if (progress >= 1) {
+
+                                    console.clear();
+                                    setTimeout(function() {
+                                        term.cyan("Benutze Proxies...\n");
+                                        term.green("-------------------------------------\n");
+                                        term.brightCyan("Made by: Epyx#4545\n");
+                                    }, 2000);
+                                    setTimeout(function() {
+                                        term.brightCyan(
+                                            "Falls du Geld für dieses Tool bezahlt hast, wurdest du verarscht :D\n"
+                                        );
+                                    }, 4000);
+                                    setTimeout(function() {
+                                        term.brightCyan(
+                                            "Es kann sehr lange dauern, bis ein gültiger Code gefunden wurde\n"
+                                        );
+                                    }, 6000);
+                                    setTimeout(function() {
+                                        term.brightCyan("Drücke jederzeit 'N' um den Generator zu stoppen\n");
+                                    }, 8000);
+                                    setTimeout(function() {
+                                        term.brightCyan("Viel Spaß :)\n");
+                                    }, 10000);
+
+                                    term.green("-------------------------------------\n");
+                                    term.green(`Discord Nitro Giftcard Generater ${version} \n`);
+                                    term.green(`Prüfe Code alle ${1 / triesPerSecond} Sekunde(n)\n`);
+
+                                    setTimeout(function() {
+
+                                        setInterval(() => {
+                                            checkCode(generatecode());
+                                        }, (1 / triesPerSecond) * 250);
+                                    }, 12000);
+
+                                } else {
+                                    setTimeout(doProgress, 100 + Math.random() * 400);
+                                }
+                            }
+
+
+                            progressBar = term.progressBar({
+                                width: 80,
+                                title: "Starte Generator....",
+                                eta: true,
+                                percent: true
+                            });
+
+                            doProgress();
+
+
+
+                        } else {
+                            term.red("'Nein' erkannt, verwende keine Proxies...\n");
+
+
+                            var progressBar, progress = 0;
+
+
+                            function doProgress() {
+
+                                // Add random progress
+                                progress += Math.random() / 10;
+                                progressBar.update(progress);
+
+                                if (progress >= 1) {
+
+                                    console.clear();
+                                    setTimeout(function() {
+
+                                        term.green("-------------------------------------\n");
+                                        term.brightCyan("Made by: Epyx#4545\n");
+                                    }, 2000);
+                                    setTimeout(function() {
+                                        term.brightCyan(
+                                            "Falls du Geld für dieses Tool bezahlt hast, wurdest du verarscht :D\n"
+                                        );
+                                    }, 4000);
+                                    setTimeout(function() {
+                                        term.brightCyan(
+                                            "Es kann sehr lange dauern, bis ein gültiger Code gefunden wurde\n"
+                                        );
+                                    }, 6000);
+                                    setTimeout(function() {
+                                        term.brightCyan("Drücke jederzeit 'N' um den Generator zu stoppen\n");
+                                    }, 8000);
+                                    setTimeout(function() {
+                                        term.brightCyan("Viel Spaß :)\n");
+                                    }, 10000);
+
+                                    term.green("-------------------------------------\n");
+                                    term.green(`Discord Nitro Giftcard Generater ${version} \n`);
+                                    term.green(`Prüfe Code alle ${12 / triesPerSecond} Sekunde(n)\n`);
+
+                                    setTimeout(function() {
+
+                                        setInterval(() => {
+                                            checkCodeOffline(generatecode());
+                                        }, (12 / triesPerSecond) * 1000);
+                                    }, 12000);
+
+                                } else {
+                                    setTimeout(doProgress, 100 + Math.random() * 400);
+                                }
+                            }
+
+
+                            progressBar = term.progressBar({
+                                width: 80,
+                                title: "Starte Generator....",
+                                eta: true,
+                                percent: true
+                            });
+
+                            doProgress();
+
+
+                        }
+                    });
+
+
+                } else {
+                    term.red("'Nein' Der Generator wird kontinuierlich Codes loggen bis er manuell gestoppt wird...\n");
+
+                    term.brightYellow(
+                        "Möchtest du Proxy-Server benutzen? [Y|N]\n"
+                    );
+
+                    term.yesOrNo({
+                        yes: ["y", "ENTER"],
+                        no: ["n"]
+                    }, function(error, result) {
+                        if (result) {
+
+
+
+                            term.cyan("Benutze Proxies...\n");
+
+                            var progressBar, progress = 0;
+
+
+                            function doProgress() {
+
+                                // Add random progress
+                                progress += Math.random() / 10;
+                                progressBar.update(progress);
+
+                                if (progress >= 1) {
+
+                                    console.clear();
+                                    setTimeout(function() {
+
+                                        term.green("-------------------------------------\n");
+                                        term.brightCyan("Made by: Epyx#4545\n");
+                                    }, 2000);
+                                    setTimeout(function() {
+                                        term.brightCyan(
+                                            "Falls du Geld für dieses Tool bezahlt hast, wurdest du verarscht :D\n"
+                                        );
+                                    }, 4000);
+                                    setTimeout(function() {
+                                        term.brightCyan(
+                                            "Es kann sehr lange dauern, bis ein gültiger Code gefunden wurde\n"
+                                        );
+                                    }, 6000);
+                                    setTimeout(function() {
+                                        term.brightCyan("Drücke jederzeit 'N' um den Generator zu stoppen\n");
+                                    }, 8000);
+                                    setTimeout(function() {
+                                        term.brightCyan("Viel Spaß :)\n");
+                                    }, 10000);
+
+                                    term.green("-------------------------------------\n");
+                                    term.green(`Discord Nitro Giftcard Generater ${version} \n`);
+                                    term.green(`Prüfe Code alle ${1 / triesPerSecond} Sekunde(n)\n`);
+
+                                    setTimeout(function() {
+
+                                        setInterval(() => {
+                                            checkCode(generatecode());
+                                        }, (1 / triesPerSecond) * 250);
+                                    }, 12000);
+
+                                } else {
+                                    setTimeout(doProgress, 100 + Math.random() * 400);
+                                }
+                            }
+
+
+                            progressBar = term.progressBar({
+                                width: 80,
+                                title: "Starte Generator....",
+                                eta: true,
+                                percent: true
+                            });
+
+                            doProgress();
+
+
+
+                        } else {
+                            term.red("'Nein' erkannt, verwende keine Proxies...\n");
+
+
+                            var progressBar, progress = 0;
+
+
+                            function doProgress() {
+
+                                // Add random progress
+                                progress += Math.random() / 10;
+                                progressBar.update(progress);
+
+                                if (progress >= 1) {
+
+                                    console.clear();
+                                    setTimeout(function() {
+
+                                        term.green("-------------------------------------\n");
+                                        term.brightCyan("Made by: Epyx#4545\n");
+                                    }, 2000);
+                                    setTimeout(function() {
+                                        term.brightCyan(
+                                            "Falls du Geld für dieses Tool bezahlt hast, wurdest du verarscht :D\n"
+                                        );
+                                    }, 4000);
+                                    setTimeout(function() {
+                                        term.brightCyan(
+                                            "Es kann sehr lange dauern, bis ein gültiger Code gefunden wurde\n"
+                                        );
+                                    }, 6000);
+                                    setTimeout(function() {
+                                        term.brightCyan("Drücke jederzeit 'N' um den Generator zu stoppen\n");
+                                    }, 8000);
+                                    setTimeout(function() {
+                                        term.brightCyan("Viel Spaß :)\n");
+                                    }, 10000);
+
+                                    term.green("-------------------------------------\n");
+                                    term.green(`Discord Nitro Giftcard Generater ${version} \n`);
+                                    term.green(`Prüfe Code alle ${12 / triesPerSecond} Sekunde(n)\n`);
+
+                                    setTimeout(function() {
+
+                                        setInterval(() => {
+                                            checkCodeOffline(generatecode());
+                                        }, (12 / triesPerSecond) * 1000);
+                                    }, 12000);
+
+                                } else {
+                                    setTimeout(doProgress, 100 + Math.random() * 400);
+                                }
+                            }
+
+
+                            progressBar = term.progressBar({
+                                width: 80,
+                                title: "Starte Generator....",
+                                eta: true,
+                                percent: true
+                            });
+
+                            doProgress();
+
+
+                        }
+                    });
+
+
+                }
+            });
+
+
+
+
+
+
+        } else {
+            term.red("'Nein' erkannt, beende Generator...\n");
+            process.exit();
+        }
+    });
+}
+
+main()
+process.on('uncaughtException', function(err) {
+    term.gray("Ein Fehler ist aufgetreten:\n");
+    term.gray(err + "\n");
+});
+
+
+// made by Epyxx
